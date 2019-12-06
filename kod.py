5b02212851686b25d466d8262b74a2c9fe9f85d3
Thekolllol90 <neosvarre@hotmail.se>
2019-12-06 10:12:48 +0100

    skrivit lite

--- kod.py
+++ kod.py
@@ -5,3 +5,20 @@ currentAttempt = ""
 
 changePassword = False
 correctPassword = False
+
+while True:
+    current = input("Write: ")[0]
+    if current == "C":
+        currentAttempt = ""
+        continue
+    elif current == "*":
+        changePassword = True
+        correctPassword = False
+        currentAttempt = ""
+        continue
+
+    currentAttempt += current
+
+    print("Current: " + currentAttempt)
+
+
