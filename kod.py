73b2510e909c7fb056a5071a710ebb7f604d446a
Thekolllol90 <neosvarre@hotmail.se>
2020-03-08 13:21:04 +0100

    its done and working

--- kod.py
+++ kod.py
@@ -1,3 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+NUMPAD = [[1, 2, 3, 'A'],
+          [4, 5, 6, 'B'],
+          [7, 8, 9, 'C'],
+          ['*', 0, '#', 'D']]
+
+ROW = [7, 11, 13, 15]
+COL = [12, 16, 18, 22]
+
+for j in range(4):
+    GPIO.setup(COL[j], GPIO.OUT)
+    GPIO.output(COL[j], 1)
+
+for i in range(4):
+    GPIO.setup(ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 password = "12345"
 passwordLength = 5
 
@@ -6,12 +26,49 @@ currentAttempt = ""
 changePassword = False
 correctPassword = False
 
+
+def numpadInput():
+    while(True):
+        for j in range(4):
+            GPIO.output(COL[j], 0)
+            for i in range(4):
+                if GPIO.input(ROW[i]) == 0:
+                    time.sleep(0.3)
+                    return str(NUMPAD[i][j])
+                    while(GPIO.input(ROW[i]) == 0):
+                        pass
+            GPIO.output(COL[j], 1)
+
+
+def open():
+    GPIO.setup(5, GPIO.OUT)
+
+    p = GPIO.PWM(5, 50)
+
+    p.start(12.5)
+    time.sleep(1)
+
+    p.stop()
+
+
+def close():
+    GPIO.setup(5, GPIO.OUT)
+
+    p = GPIO.PWM(5, 50)
+
+    p.start(2.5)
+    time.sleep(1)
+
+    p.stop()
+
+
+close()
 while True:
-    current = input("Write: ")[0]
+    current = numpadInput()
     if current == "C":
-        continue
         currentAttempt = ""
-    elif current == "*":
+        continue
+    elif current == '*':
         changePassword = True
         correctPassword = False
         currentAttempt = ""
@@ -19,22 +76,19 @@ while True:
 
     currentAttempt += current
 
-    print("Current: " + currentAttempt)
-
     if changePassword and correctPassword:
         if len(currentAttempt) == passwordLength:
             password = currentAttempt
             currentAttempt = ""
-            print("Changed password to " + password)
             changePassword = False
             correctPassword = False
     else:
         if len(currentAttempt) == passwordLength:
             if currentAttempt == password:
-                print("Correct")
+                open()
                 currentAttempt = ""
                 correctPassword = True
             else:
-                print("Incorrect")
+                close()
                 currentAttempt = ""
                 correctPassword = False
